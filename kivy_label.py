aae15c201d0e168ba1c803067de7f86564b60036
Tiago Ribeiro Santos <tiago.programador@hotmail.com>
2018-12-04 19:11:34 -0200

    mudei a classe

--- kivy_label.py
+++ kivy_label.py
@@ -4,9 +4,9 @@ import kivy
 from kivy.uix.label import Label
 from kivy.app import App
 
-class MeuAplicativo(App):
+class Aplicativo(App):
     def build(self):
         return Label(text='TIAGOOO!')
 
 if __name__ =='__main__':
-    MeuAplicativo().run()
+    Aplicativo().run()
